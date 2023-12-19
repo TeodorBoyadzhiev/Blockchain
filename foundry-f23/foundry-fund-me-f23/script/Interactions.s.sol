f8a4ded8397e4e825c8aeed5d60875bd92c9f6d8
Teodor <teodor1497@gmail.com>
2023-12-19 15:30:13 +0200

    Add FundMe contract testing

---
+++ foundry-f23/foundry-fund-me-f23/script/Interactions.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract Interactions is Script {
+    function run() external {}
+}
+
+contract WithdrawFundMe is Script {}
